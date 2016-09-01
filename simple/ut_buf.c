2a4b95b202f1cfda86d6ec2ba7d192b6e997cff7
rdentato <rdentato@gmail.com>
2016-09-01 18:16:14 +0200

    Test for the buf_t type

--- simple/ut_buf.c
+++ simple/ut_buf.c
@@ -49,8 +49,55 @@ int main(int argc, char *argv[])
   while (bufreadln(b,0,f)) {
     logprintf("[%s]",buf(b));
   }
-  
   fclose(f);
+  
+  
+  bufsets(b,0,"WXYZ");
+  logcheck(bufgetc(b,4) == '\0');
+  logcheck(buflen(b) == 4);
+  logcheck(strcmp(buf(b),"WXYZ")==0);
+  
+  bufsets(b,2,"CDE");
+  logcheck(bufgetc(b,5) == '\0');
+  logcheck(buflen(b) == 5);
+  logcheck(strcmp(buf(b),"WXCDE")==0);
+  
+  bufinsc(b,2,'A');
+  logcheck(bufgetc(b,6) == '\0');
+  logcheck(buflen(b) == 6);
+  if (!logcheck(strcmp(buf(b),"WXACDE")==0)) {
+    logprintf("   [%s]",buf(b));
+  }
+
+  bufinss(b,3,"]|[");
+  logcheck(buflen(b) == 9);
+  logcheck(strcmp(buf(b),"WXA]|[CDE")==0);
+
+  bufsets(b,buflen(b),"]...");
+  logcheck(buflen(b) == 13);
+  logcheck(strcmp(buf(b),"WXA]|[CDE]...")==0);
+
+  bufinss(b,0,"...[");
+  logcheck(buflen(b) == 17);
+  logcheck(strcmp(buf(b),"...[WXA]|[CDE]...")==0);
+
+  logprintf("   [%s]",buf(b));
+  bufdel(b,0,2);
+  logcheck(buflen(b) == 14);
+  if (!logcheck(strcmp(buf(b),"[WXA]|[CDE]...")==0)) {
+    logprintf("   [%s]",buf(b));
+  }
+  
+  bufdel(b,11,10000);
+  logcheck(buflen(b) == 11);
+  logcheck(strcmp(buf(b),"[WXA]|[CDE]")==0);
+  
+  bufdel(b,5,5);
+  logcheck(buflen(b) == 10);
+  if (!logcheck(strcmp(buf(b),"[WXA][CDE]")==0)) {
+    logprintf("   [%s]",buf(b));
+  }
+  
   logclose();
   exit(0);
 }
