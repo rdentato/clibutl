d85029a098a5ce56fd948e5f1811fe8f9ad8abcb
rdentato <rdentato@gmail.com>
2016-09-04 22:04:06 +0200

    Check that logassert() fails properly

---
+++ test/ut_logassert.c
@@ -0,0 +1,12 @@
+#define UTL_MAIN
+#include "utl.h"
+
+int main(int argc, char *argv[])
+{
+  logopen("t_logassert.log","w");
+  
+  logassert(5>10);
+  logassert(utl_ret(0));
+  logcheck(3/utl_ret(0) == 0);
+  logclose();
+}
