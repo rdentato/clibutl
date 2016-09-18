e8c69b4279a9b2953f03f52eb1c4862552d2d566
rdentato <rdentato@gmail.com>
2016-09-18 18:22:49 +0200

    Added documentation from utl_pmx.h

--- src/utl_pmx.c
+++ src/utl_pmx.c
@@ -55,28 +55,12 @@ exceptions:
   - `|` Separates alternatives.
   - `%` Escapes next character (`%(` is the character `(` itself)
 
+### Alternatives
 
-### Character class recognizers
-
-  These patterns match characters that are within commonly used sets.
-
-  - `a`  (isalpha)	matches an alphabetic character
-  - `c`  (iscntrl)	matches a control character
-  - `d`  (isdigit)	matches a digit. Not locale-specific.
-  - `g`  (isgraph)	matches a graphic character, excluding the space character.
-  - `i`  (isascii) matches a character whose code is between 0 and 127
-  - `k`  (isblank)	matches a blank character 
-  - `l`  (islower)	matches a lowercase character
-  - `p`  (ispunct)	matches a punctuation character
-  - `q`  (isalnum)	matches an alphanumeric character
-  - `r`  (isprint)	matches a printable character, including the space character.
-  - `s`  (isspace)	matches a space character
-  - `u`  (isupper)	matches an uppercase character
-  - `w`  (isword)) matches an alphanumeric character or and underscore ''|_|'
-  - `x`  (isxdigit) matches an hexadecimal digit. Not locale-specific.
-  - `.`  matches any character except `\0` and `\n` .
+  To match one of alternative patterns use the '|' character. For example:
   
-
+  - `a(b|c)|d` matches `ab` `ac` or `d` 
+   
 ### Recognizer modifiers
    
   - `!`      negates the recognizer: `<!d>` is any character that is not a digit
@@ -88,13 +72,106 @@ exceptions:
   - -*m*     matches at most *m* times
   - *n*-*m*  matches from *n* to *m* times
   
- Examples:
-  - `<3d>`   matches 3 digits
-  - `<3!d>`  matches 3 non-digits
-  - `<+>(ab|cd)`
-             matches strings like: "abab" "abcdab" "cdabcdcd" etc
-  - `<-3=xyz>` matches "xy" "xx" "zyx" "xxx" "z" ""
+  Modifiers can be applied to sub-expressions:
   
+  - `<3>(ab|xy)` matches "`abxyab`" or "`xyxyxy`" or "`xyabab`" etc..
+  
+  there must be no space between `>` and `(`.
+  
+  Note that the meaning of `!` depends on the usage:
+  - for characters class, it will match a character that is not in that class
+  - for a sub-expressions, the match will be empty
+  
+  For example:
+  - `a<!d><!d>`          will match any 3 character string starting 'a' follewed by two non-digit character
+  - `a<!>(bc|de)<2l>` will match any 3 lower case letter string starting with `a` except `abc` and `ade`
+  
+  In the first example, `<!d>` consumes one character in the text, in the second example, `<!>(bc|de)` does
+not consume any text.
+  
+### Class recognizers
+
+  These patterns match classes of strings that are commonly used.
+
+  These will use the corresponding `C` functions.
+ 
+  - `a`  (isalpha)	matches an alphabetic character
+  - `s`  (isspace)	matches a space character
+  - `u`  (isupper)	matches an uppercase character
+  - `l`  (islower)	matches a lowercase character
+  - `d`  (isdigit)	matches a digit.
+  - `x`  (isxdigit) matches an hexadecimal digit.
+  - `w`  (isalnum)	matches an alphanumeric character
+  - `c`  (iscntrl)	matches a control character
+  - `g`  (isgraph)	matches a graphic character, excluding the space character.
+  - `p`  (ispunct)	matches a punctuation character
+  - `r`  (isprint)	matches a printable character, including the space character.
+  - `i`  (isascii)  matches a character whose code is between 0 and 127
+
+  These are defined within `pmx`:
+
+  - `h`  (hspace)   matches an horizontal space (` `, `\t` and `nbsp`)
+  - `.`  (any)      matches any character except `\0` and `\n` .
+  - `$`  (eot)      matches the end of the text
+  - `N`  (eol)      matches the end of the line that can either be `\n` or `\r\n`
+  - `Q`  (quoted)   matches a quoted string with `\` as escape character
+  - `B`  (braced)   matches a text enclosed in (balanced) braces.
+
+  The `<Q>` and `<B>` patterns allow for the definition of the delimiters and the
+escape character. If used by themselves they will try to "guess" from the text the
+delimiters.
+
+   For `<B>`:
+    - `( )`               Parenthesis
+    - `[ ]`               Square brackets
+    - `{ }`               Curly braces
+    - `< >`               Angular braces
+    - `« »`               French Guillemot
+    - `&#2329; &#232A;`   Unicode ANGLE BRACKETS 
+    - `&#27E8; &#27E9;`   Unicode MATHEMATICAL ANGLE BRACKETS 
+    - `&#27EA; &#27EB;`   Unicode MATHEMATICAL DOUBLE ANGLE BRACKETS 
+
+    For `<Q>`
+    - `" "`               Double quotes
+    - `' '`               Single quotes
+    - `` ` ` ``           Back quotes
+    - `« »`               French Guillemot
+    - `&#2018; &#2019;    Unicode single quotes 
+    - `&#201C; &#201D;    Unicode double quotes 
+  
+  Examples:
+  
+  - `<3d>`         matches 3 digits
+  - `<3!d>`        matches 3 non-digits
+  - `<+>(ab|cd)`   matches strings like: "abab" "abcdab" "cdabcdcd" etc
+  - `<-3=xyz>`     matches "xy" "xx" "zyx" "xxx" "z" ""
+  - `0<d>`         matches two digits numbers that start with `0`: `04`, `09`, `00`
+  - `<B>L`         matches strings like `(bcd)L`, `{b{cd}}L`, `<<<bcd>ab>x>L`
+  - `<Q>`          matches strings like `"bcd"`, `"BA\"XY"`, `'art'`
+  - `<utf><Q«»@>`  matches strings like `«bcd»`, `«BA@»XY»`, `«art»`
+
+### User defined character classes
+
+  It is possible to match a character that is in in a given set as follows:
+
+ - `<=...>`  characters list
+ - `<#...>`  characters codes (in hex)
+  
+ Example:
+ 
+ - `<=A-Ga-g>`     matches one of the following characters: `ABCDEFGabcdefg`
+ - `<!=xyz>`       matches anything except the characters `x`, `y` and `z`
+ - `<#0370-03FF>`  matches Greek and Coptic characters
+ - `<=-+>`         matches `+` or `-` (must be the first character in the set)
+ - `<=])}>>`       matches a closing bracket including `>` (must be the last one)
+
+  
+### Extendin recognizers
+  
+  
+  - `:`' : if (utl_pmx_ext)
+    
+
 ### Text encoding
   
   By default, pmx assumes that strings are encoded with some 8-bit scheme (e.g. ISO-8859-x)
@@ -126,37 +203,6 @@ the encoding to avoid unexpected results. Assuming the text is in UTF-8:
    pmxsearch("<iso><.>x","uàx");  // will match two bytes
   ```
   
-
-### User defined character classes
-
-  It is possible to match a character that is in in a given set as follows:
-
- - `<=...>`  characters list
- - `<#...>`  characters codes (in hex)
-  
- Example:
- 
- - `<=A-Ga-g>`     matches one of the following characters: `ABCDEFGabcdefg`
- - `<!=xyz>`       matches anything except the characters `x`, `y` and `z`
- - `<#0370-03FF>`  matches Greek and Coptic characters
- - `<=-+>`         matches `+` or `-` (must be the first character in the set)
- - `<=])}>>`       matches a closing bracket including `>` (must be the last one)
- 
-  
-### Alternatives
-
-  To match one of alternative patterns use the '|' character. For example:
-  
-  - `a(b|c)|d` matches `ab` `ac` or `d` 
- ..
-   
-  If the characters '&', '>' or '$' are part of the strings specified,
-they must be preceeded by '&'.  For example:
-
- .['|<$a&$b$c&>d>'] matches ''|a$b|' or  ''|c>d|'.
- ..
-   
-  
 .%% Submatching captures
 ''''''''''''''''''''''''
   Submatches can be denoted using parenthesis. There can be up to 9 
@@ -173,6 +219,9 @@ submatches. Submatches can be nested. Examples:
 
 .%% Back references
 '''''''''''''''''''
+
+  - `^`' : if (inv) utl_pmx_set_paterror(pat); inv = 0;
+
   It is possible to refer to captured test with '|<1>|, '|<2>|, ...,  '|<9>|.
   
   For example: '|(<u>)<1>| matches the doubling of an upper case character as
@@ -181,35 +230,12 @@ submatches. Submatches can be nested. Examples:
 
 .%% Recognizers
 '''''''''''''''
-  Recognizers are patterns that match a specific object (for example
-"'|&q|" for a quoted string).
-  
-  .['|&b|'/xy/] Braced string. Matches a string enclosed by the characters
-               /x/ and /y/.
-               
-               If /x/ and /y/ are different there must be a balanced number
-               of delimiters to complete the match.  For example the 
-               pattern "'|%b()|" matches the entire string
-               "'|(defun f (x) (+ x 1))|".
-               
-               If /x/ and /y/ are equal, the match stops at the first occurence
-               of the delimiter character.
-               
-               The recognires '|&b| honours the escape character set with '|&e|
-               (see below)
                 
    ['|&d|]     A possibly signed integer (e.g. : "'|32|", "'|-15|", "'|+7|")
    ['|&f|]     A possibly signed decimal number. It will also match integers
                (e.g. : '|3.2|, '|-.5|, '|+7|).
                
    ['|&l|]     The rest of the line up to the newline character(s) (see below)
-   ['|&n|]     The end of the line. Matches any of "'|\r\n|", "'|\r|" or "'|\n|"
-   ['|&q|]     A single or double quoted string (including the quotes). To
-               match just one type of quoted string use the "'|&b|" recognizer
-               (e.g. "'|&b'`'|" for single quoted strings).
- 
-               The recognizer '|&q| honours the escape character set with '|&e|
-               (see below).
                
    ['|&x|]     An hexadecimal number (e.g. "'|C1A0|")
   ..
@@ -811,6 +837,7 @@ static int utl_pmx_class(char **pat_ptr, char **txt_ptr)
     case 'u' : utl_W(isupper(ch))               ; break;
     case 'l' : utl_W(islower(ch))               ; break;
     case 'd' : utl_W(isdigit(ch))               ; break;
+    case 'k' : utl_W(isblank(ch))               ; break;
     case 'x' : utl_W(isxdigit(ch))              ; break;
     case 'w' : utl_W(isalnum(ch))               ; break;
     case 'c' : utl_W(iscntrl(ch))               ; break;
