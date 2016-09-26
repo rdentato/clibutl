2acf576ff9af2870daf13b51d53e2664188ae067
rdentato <rdentato@gmail.com>
2016-09-27 00:33:47 +0200

    Added Header

--- src/utl_mem.h
+++ src/utl_mem.h
@@ -1,5 +1,17 @@
-/*  .% Traced memory
-**  ================
+/* 
+**  (C) 2014 by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+**     
+**                     ___   __
+**                  __/  /_ /  )
+**          ___  __(_   ___)  /
+**         /  / /  )/  /  /  /  Minimalist
+**        /  /_/  //  (__/  /  C utility 
+**       (____,__/(_____(__/  Library
+**
+
 
 **      ______   ______  ______ 
 **     /      \ / ___  )/      \
