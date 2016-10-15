f007f86c74acf919fdf9a0b04df8863055f508ad
rdentato <rdentato@gmail.com>
2016-10-15 23:28:20 +0200

    Unit test on trace

---
+++ test/ut_trc.c
@@ -0,0 +1,29 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+**     
+**                        ___   __
+**                     __/  /_ /  )
+**             ___  __(_   ___)  /
+**            /  / /  )/  /  /  /
+**           /  (_/  //  (__/  / 
+**          (____,__/(_____(__/
+**    https://github.com/rdentato/clibutl
+**
+*/
+
+#include "utl.h"
+
+
+int main(int argc, char *argv[])
+{
+  logopen("l_trc.log","w");
+  
+  logtrace("pippo","pluto");
+  logtracecheck("pippo","pluto");
+  
+  logclose();
+  exit(0);
+}
