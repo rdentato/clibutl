9ae505b7cee53beda55f406f3cb8c604f62b422f
rdentato <rdentato@gmail.com>
2016-11-20 08:51:30 +0100

    Make explicit that I used a dirty trick for counting a FAIL test as a success

--- test/ut_trc.c
+++ test/ut_trc.c
@@ -19,24 +19,33 @@
 
 int main(int argc, char *argv[])
 {
+  int chk ;
+  
   logopen("l_trc.log","w");
   
   logtracewatch("pluto") {
     logtrace("pluto");
   }
 
+  chk = utl_log_check_fail;
   logtracewatch("event<*s>1", "event2") {
     logtrace("event 1");
     logtrace("event 2");
     logtrace("event 1");
   }
+  logprintf("In the watch above a test is supposed to FAIL! (and will not be counted)");
+  if (chk == utl_log_check_fail-1) utl_log_check_fail--;
+  else utl_log_check_fail++;
   
   logtracewatch("pippo", "<not>pluto") {
+    chk = utl_log_check_fail;
     logtrace("pluto");
+    logprintf("The check above is supposed to FAIL! (and will not be counted)");
+    if (chk < utl_log_check_fail) utl_log_check_fail--;
+    else utl_log_check_fail++;
     logtrace("pippo");
   }
-  /* this is to recognize that 2 fails were expected */
-  if (utl_log_check_fail >= 2) utl_log_check_fail -= 2;
+  
   logclose();
   exit(0);
 }
