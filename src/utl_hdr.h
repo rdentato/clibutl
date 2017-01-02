49b73d407584a967a1edc8ad50e4cb343f408ead
rdentato <rdentato@gmail.com>
2017-01-02 11:04:54 +0100

    Added macros to handle default values for variadic macros

--- src/utl_hdr.h
+++ src/utl_hdr.h
@@ -55,5 +55,8 @@ void *utl_retptr(void *x);
 
 extern const char *utl_emptystring;
 
+#define utl_arg0(x1,...)        x1                                                                                                                             
+#define utl_arg1(x1,x2,...)     x2
+#define utl_arg2(x1,x2,x3,...)  x3
 
 //>>>//
