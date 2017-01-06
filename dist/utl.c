5b94c4eec777df706917afdb771e30d0f54c7696
rdentato <rdentato@gmail.com>
2017-01-06 13:28:54 +0100

    Latest Version

--- dist/utl.c
+++ dist/utl.c
@@ -21,6 +21,66 @@ const char *utl_emptystring = "";
 
 int   utl_ret(int x)      {return x;}
 void *utl_retptr(void *x) {return x;}
+
+/* * Collection of hash functions * */
+
+/* Bob Jenkins' "one_at_a_time" hash function
+**  http://burtleburtle.net/bob/hash/doobs.html
+*/
+uint32_t utl_hash_string(void *key)
+{
+  uint32_t h = 0x071f9f8f;
+  uint8_t *k = key;
+  
+  while (*k)  {
+    h += *k++;
+    h += (h << 10);
+    h ^= (h >> 6);
+  }
+  h += (h << 3);
+  h ^= (h >> 11);
+  h += (h << 15);
+  
+  return h;
+}
+
+/* Bob Jenkins' integer hash function
+**  http://burtleburtle.net/bob/hash/integer.html
+*/
+
+uint32_t utl_hash_int32(void *key)
+{
+  uint32_t h = *((uint32_t *)key);
+  h = (h+0x7ed55d16) + (h<<12);
+  h = (h^0xc761c23c) ^ (h>>19);
+  h = (h+0x165667b1) + (h<<5);
+  h = (h+0xd3a2646c) ^ (h<<9);
+  h = (h+0xfd7046c5) + (h<<3);
+  h = (h^0xb55a4f09) ^ (h>>16);
+  return h;
+}
+
+/* Quick and dirty PRNG */
+/*
+uint32_t utl_rnd()
+{ // xorshift
+  static uint32_t rnd = 0;
+  while (rnd == 0) rnd = (uint32_t)time(0);
+	rnd ^= rnd << 13;
+	rnd ^= rnd >> 17;
+	rnd ^= rnd << 5;
+	return rnd;
+}
+*/
+/*
+uint32_t utl_rnd()
+{ // Linear Congruetial
+  static uint32_t rnd = 0;
+  if (rnd == 0) rnd = (uint32_t)time(0);
+	rnd = 1664525 * rnd + 1013904223;
+	return rnd;
+}
+*/
 #line 286 "src/utl_log.c"
 #ifndef UTL_NOLOG
 #ifdef UTL_MAIN
@@ -301,11 +361,11 @@ size_t utl_mem_used(void) {return utl_mem_allocated;}
 
 #endif
 #endif
-#line 21 "src/utl_vec.c"
+#line 20 "src/utl_vec.c"
 #ifndef UTL_NOVEC
 #ifdef UTL_MAIN
 
-int utl_vec_nullcmp(void *a, void *b){return 0;}
+int utl_vec_nullcmp(void *a, void *b, void* aux){return 0;}
 
 static int16_t utl_vec_makeroom(vec_t v,uint32_t n)
 {
@@ -317,7 +377,8 @@ static int16_t utl_vec_makeroom(vec_t v,uint32_t n)
   while (new_max <= n) new_max += (new_max / 2);  /*  (new_max *= 1.5) instead of (new_max *= 2) */
   new_vec = (uint8_t *)realloc(v->vec, new_max * v->esz);
   if (!new_vec) return 0;
-  v->vec = new_vec;  v->max = new_max;
+  v->vec = new_vec;
+  v->max = new_max;
   return 1;
 }
 
@@ -361,21 +422,49 @@ static int16_t utl_vec_delgap(vec_t v, uint32_t i, uint32_t l)
   return 1;
 }
 
+int16_t utl_vec_del(vec_t v, uint32_t i)
+{
+  return utl_vec_delgap(v,i,1);
+}
 
-vec_t utl_vec_new(uint16_t esz)
+int16_t utl_vec_delrange(vec_t v, uint32_t i,  uint32_t j)
+{
+  if (j<i) return 0;
+  return utl_vec_delgap(v,i,j-i+1);  
+}
+
+vec_t utl_vec_new(uint16_t esz, int (*cmp)(void *, void *,void *), uint32_t (*hsh)(void *, void *))
 {
   vec_t v = NULL;
-  uint32_t sz = sizeof(vec_s)+(esz-4); // 4 is the size of the elm array
+  uint32_t sz;
+    
+  if (hsh) {
+    if (!cmp) return NULL;
+    esz += (4+(esz & ~0x03)) & 0x03; // ensure is aligned to 4 bytes
+    esz += sizeof(uint32_t); // To store the key hash value
+  }
   
+  sz = sizeof(vec_s)+(esz-4); // 4 is the size of the elm array
   v = (vec_t)malloc(sz);
   if (v) {
     memset(v,0,sz);
     v->esz = esz;
     v->max = vec_MIN_ELEM;
     v->vec = (uint8_t *)malloc(v->max * esz);
-    v->elm = ((char *)v) + offsetof(vec_s,eld);
-    vecunsorted(v);
-    if (!v->vec) { free(v); v = NULL;}
+    if (!v->vec) { 
+      free(v);  v = NULL;
+    }
+    else {
+      v->elm = ((char *)v) + offsetof(vec_s,eld);
+      v->cmp = cmp;
+      v->hsh = hsh;
+      memset(v->vec,0xFF,v->max *esz); // if hashed, hash would be 0xFFFFFFFF
+      vecunsorted(v);
+      v->fst=vec_MAX_CNT;
+      v->lst=vec_MAX_CNT;
+      v->cur=0;
+      v->aux=NULL;
+    }
   }
   return v;
 }
@@ -426,6 +515,185 @@ void *utl_vec_ins(vec_t v, uint32_t i)
   return elm;
 }
 
+/* * Sorted sets  * */
+
+/*                             
+#define utl_dpqswap(a,b) do { if (a!=b) { \
+                                uint8_t t;\
+                                pa = ((uint8_t *)a); pb = ((uint8_t *)b); \
+                                for (uint32_t k=0; k<esz; k++,pa++,pb++) { \
+                                  t = *pa; *pa = *pb; *pb = t;\
+                                }\
+                              }\
+                           } while (0)
+*/
+/*
+#define utl_dpqswap(a,b) do { if (a!=b) { \
+                                  uint32_t sz = esz;\
+                                  uint8_t  tmp8; \
+                                  uint32_t tmp32; \
+                                  uint8_t *pa = ((uint8_t *)a); \
+                                  uint8_t *pb = ((uint8_t *)b); \
+                                  while (sz >= 4) { \
+                                    tmp32 = *(uint32_t *)pa; \
+                                    *(uint32_t *)pa = *(uint32_t *)pb;\
+                                    *(uint32_t *)pb = tmp32;\
+                                    sz-=4; pa+=4; pb+=4;\
+                                  }\
+                                  switch (sz) {\
+                                    case 3: tmp8=*pa; *pa=*pb; *pb=tmp8; pa--; pb--;\
+                                    case 2: tmp8=*pa; *pa=*pb; *pb=tmp8; pa--; pb--;\
+                                    case 1: tmp8=*pa; *pa=*pb; *pb=tmp8; pa--; pb--;\
+                                  }\
+                                }\
+                           } while (0)
+*/                           
+static inline void utl_dpqswap(void *a, void *b, uint32_t sz)
+{
+  uint8_t  tmp8;
+  uint32_t tmp32;
+  uint8_t *pa = ((uint8_t *)a);
+  uint8_t *pb = ((uint8_t *)b);
+  
+  if (a!=b) {
+    while (sz >= 4) {
+      tmp32 = *(uint32_t *)pa;
+      *(uint32_t *)pa = *(uint32_t *)pb;
+      *(uint32_t *)pb = tmp32;
+      sz-=4; pa+=4; pb+=4;
+    }
+    switch (sz) {
+      case 3: tmp8=*pa; *pa=*pb; *pb=tmp8; pa--; pb--;
+      case 2: tmp8=*pa; *pa=*pb; *pb=tmp8; pa--; pb--;
+      case 1: tmp8=*pa; *pa=*pb; *pb=tmp8; pa--; pb--;
+    }
+  }
+}
+
+static inline int utl_dpqordrange(int32_t a, int32_t b, int32_t c)
+{
+  if (a>b) {
+    if (b>c) return 123;  // a>b>c
+    if (c>a) return 312;  // c>a>b
+             return 132;  // a>c>b
+  } 
+  if (a>c) return 213;    // b>a>c
+  if (c>b) return 321;    // c>b>a 
+           return 231;    // b>c>a
+}
+
+static uint32_t utl_rnd_status = 0;
+static inline uint32_t utl_dpqrand() 
+{ // xorshift
+  if (utl_rnd_status == 0) utl_rnd_status = (uint32_t)time(0);
+	//utl_rnd_status = 1664525 * utl_rnd_status + 1013904223; // LCRNG 
+  utl_rnd_status ^= utl_rnd_status << 13;
+	utl_rnd_status ^= utl_rnd_status >> 17;
+	utl_rnd_status ^= utl_rnd_status << 5;
+	return utl_rnd_status;
+}
+
+#define utl_dpqptr(k)    ((uint8_t *)base+(k)*esz)
+#define utl_dpqpush(l,r) do {stack[stack_top][0]=(l); stack[stack_top][1]=(r); stack_top++; } while(0)
+#define utl_dpqpop(l,r)  do {stack_top--; l=stack[stack_top][0]; r=stack[stack_top][1];} while(0)
+
+void utl_dpqsort(void *base, uint32_t nel, uint32_t esz, int (*cmp)(const void *, const void *, const void *), void *aux)
+{
+  int32_t left,right;
+  uint8_t *leftptr, *rightptr; 
+  uint32_t L,K,G;
+  
+  int32_t stack[128][2]; // Enough for 2^31 max elements in the array
+  int16_t stack_top = 0;
+  //uint32_t stack_max = 0;
+  
+  utl_dpqpush(0,nel-1);
+  while (stack_top > 0) {
+    //if (stack_top > stack_max) stack_max=stack_top;
+    utl_dpqpop(left, right);
+    if (left < right) {
+      if ((right - left) <= 16) {  // Use insertion sort
+        for (int32_t i = left+1; i<=right; i++) {
+          rightptr = utl_dpqptr(i);
+          leftptr = rightptr - esz;
+          for (int32_t j=i; j>0 && cmp(leftptr, rightptr, aux) > 0; j--) {
+            utl_dpqswap(rightptr, leftptr, esz);
+            rightptr = leftptr;
+            leftptr = rightptr - esz;
+          }
+        }
+      }
+      else {
+        leftptr = utl_dpqptr(left);
+        rightptr = utl_dpqptr(right);
+        
+        /* Randomize pivot to avoid worst case (already sorted array) */
+        L = left + (utl_dpqrand() % (right-left));
+        G = left + (utl_dpqrand() % (right-left));
+        utl_dpqswap(utl_dpqptr(L),leftptr, esz);
+        utl_dpqswap(utl_dpqptr(G),rightptr, esz);
+        
+        if (cmp(leftptr, rightptr, aux) > 0) {
+          utl_dpqswap(leftptr, rightptr, esz);
+        }
+        L=left+1; K=L; G=right-1;
+        while (K <= G) {
+          if (cmp(utl_dpqptr(K), leftptr, aux) < 0) {
+            utl_dpqswap(utl_dpqptr(K), utl_dpqptr(L), esz);
+            L++;
+          }
+          else if (cmp(utl_dpqptr(K), rightptr, aux) > 0) {
+            while ((cmp(utl_dpqptr(G), rightptr, aux) > 0) && (K<G)) 
+              G--;
+
+            utl_dpqswap(utl_dpqptr(K), utl_dpqptr(G), esz);
+            G--;
+            if (cmp(utl_dpqptr(K), leftptr, aux) < 0) {
+              utl_dpqswap(utl_dpqptr(K), utl_dpqptr(L), esz);
+              L++;
+            }
+          }
+          K++;
+        }
+        L--; G++;
+
+        utl_dpqswap(leftptr,  utl_dpqptr(L), esz);
+        utl_dpqswap(rightptr, utl_dpqptr(G), esz);
+
+        switch (utl_dpqordrange((right-(G+1)), ((G-1)-(L+1)), ((L-1)-left))) {
+          case 123: utl_dpqpush(G+1, right); utl_dpqpush(L+1, G-1);   utl_dpqpush(left, L-1);  break;
+          case 132: utl_dpqpush(G+1, right); utl_dpqpush(left, L-1);  utl_dpqpush(L+1, G-1);   break;
+          case 213: utl_dpqpush(L+1, G-1);   utl_dpqpush(G+1, right); utl_dpqpush(left, L-1);  break;
+          case 231: utl_dpqpush(L+1, G-1);   utl_dpqpush(left, L-1);  utl_dpqpush(G+1, right); break;
+          case 312: utl_dpqpush(left, L-1);  utl_dpqpush(G+1, right); utl_dpqpush(L+1, G-1);   break;
+          case 321: utl_dpqpush(left, L-1);  utl_dpqpush(L+1, G-1);   utl_dpqpush(G+1, right); break;
+        }
+      }
+    }
+  }
+  //logtrace("DPQ: maxstack: %d",stack_max);
+}
+
+void utl_vec_sort(vec_t v, int (*cmp)(void *, void *, void *))
+{
+  if (v->hsh) {vecunsorted(v); return;}
+  
+  if (cmp != utl_vec_nullcmp && cmp != v->cmp) {
+    v->cmp = cmp;
+    vecunsorted(v);
+  }
+  if (v->cmp == NULL) {
+    vecunsorted(v);
+  }
+  else if (!vecissorted(v)) {
+    if (v->cnt > 1) {
+      //qsort(v->vec,v->cnt,v->esz,(int (*)(const void *, const void *))(v->cmp));  
+      utl_dpqsort(v->vec,v->cnt,v->esz,(int (*)(const void *, const void *, const void *))(v->cmp),v);  
+    }
+    vecsorted(v);
+  }
+}
+
 static void *utl_vec_add_sorted(vec_t v)
 {
   uint8_t *elm=NULL;
@@ -437,7 +705,7 @@ static void *utl_vec_add_sorted(vec_t v)
   while (lo <= hi) {
     mid = lo + (hi-lo)/2;
     elm = v->vec + mid * v->esz;
-    ret = v->cmp(v->elm,elm);
+    ret = v->cmp(v->elm,elm,v);
     if (ret == 0) {
       memcpy(elm, v->elm, v->esz);
       return elm;
@@ -451,22 +719,168 @@ static void *utl_vec_add_sorted(vec_t v)
   return utl_vec_ins(v,lo);
 }
 
+static void *utl_vec_search_sorted(vec_t v)
+{
+  uint8_t *elm=NULL;
+  int32_t mid,lo,hi;
+  int ret;
+  
+  vecsort(v); 
+  lo = 0; hi = v->cnt-1;
+  while (lo <= hi) {
+    mid = lo + (hi-lo)/2;
+    elm = v->vec + mid * v->esz;
+    ret = v->cmp(v->elm,elm,v);
+    if (ret == 0) return elm;
+    if (ret < 0) hi = mid-1;
+    else         lo = mid+1;
+  }
+  return NULL;
+}
+
+/* * Unsorted sets (Hash tables) * */
+
+#define utl_h(p,sz) *((uint32_t *)(((uint8_t *)(p))+(sz)-sizeof(uint32_t)))
+
+static void *utl_remove_hashed(vec_t v,uint32_t pos)
+{
+  uint32_t nxt;
+  uint32_t elm_h;
+  uint8_t  *elm;
+  uint8_t  *val;
+  
+  while(1) {
+    val = v->vec + pos*v->esz;
+    memset(val,0xFF,v->esz);
+    
+    nxt = (pos+1) & (v->max-1);
+    elm = v->vec + nxt*v->esz;
+    elm_h = utl_h(elm,v->esz);
+    if (elm_h == 0xFFFFFFFF) break; // empty spot
+    if ((elm_h & (v->max-1)) == nxt) break; // with delta 0
+    memcpy(val, elm, v->esz);
+    pos = nxt;
+  }
+  v->cnt--;
+  return elm;
+}
+
+static void *utl_search_hashed(vec_t v)
+{
+  uint8_t *elm = NULL;
+  uint32_t pos;
+  uint32_t val_h;
+  uint32_t elm_h;
+  uint32_t max = v->max;
+  int32_t delta;
+  
+  val_h = v->hsh(v->elm,v) & 0x7FFFFFFF;
+  pos = val_h;
+  while(1) {
+    delta = 0;
+    pos &= (max-1);
+    elm = v->vec + pos * v->esz;
+    elm_h = utl_h(elm,v->esz);
+    if (elm_h == 0xFFFFFFFF) return NULL;
+    if ((elm_h == val_h) && (v->cmp(v->elm,elm,v) == 0)) return elm;
+    if ((elm_h - (utl_h(elm,v->esz) & (max-1))) < delta) return NULL;
+    pos++; delta++;
+  }
+  v->cnt--;
+  return elm; 
+}
+
+static uint8_t *utl_vec_hsh_set(uint8_t *val, uint8_t *tab, uint32_t max, uint16_t esz,  int (*cmp)(void *, void *,void *), void *aux)
+{
+  uint32_t pos;
+  uint32_t prv;
+  uint8_t *elm = NULL;
+  int32_t delta;
+  uint32_t val_h;
+  uint32_t elm_h;
+  
+  val_h = utl_h(val,esz);
+  if (val_h < 0x80000000) {
+    delta = 0;
+    pos = val_h;
+    while (1) {    
+      pos &= (max-1);
+      //logtrace("pos: %d hash: %08X max: %d",pos,h,max);
+      elm = tab + pos * esz;
+      elm_h = utl_h(elm,esz);
+      if ((elm_h == 0xFFFFFFFF) || ((elm_h == val_h) && (cmp(val,elm,aux) == 0)))  {
+        memcpy(elm,val,esz);
+        break;
+      }
+      pos++; delta++;
+    } 
+    // Robin Hood: swap with previous if delta is lower
+    while(delta > 0) {
+      prv = (pos+max-1) & (max-1);
+      elm = tab + prv * esz;
+      // logtrace("RH: (delta: %d pos: %d) ( deltap: %d prev: %d)",delta,pos,(prv - (utl_h(elm,esz) & (max-1))),prv);
+      delta--;
+      if ((prv - (utl_h(elm,esz) & (max-1))) > delta) break;
+      memcpy(tab+pos*esz, tab+prv*esz, esz);
+      memcpy(elm, val, esz);
+      pos = prv;
+    }
+  }
+  return elm;
+}
+
+static uint8_t *utl_reash(uint8_t *tab,uint32_t max, uint16_t esz, int (*cmp)(void *, void *, void *),void *aux)
+{
+  uint8_t *new_tab;
+  uint32_t new_max;
+  uint32_t k;
+  
+  new_max = max * 2;
+  new_tab = malloc(new_max * esz);
+  if (new_tab) {
+    memset(new_tab,0xFF,new_max*esz);
+    for (k=0; k<max;k++) {
+      //logtrace("reash %d",k);
+      utl_vec_hsh_set(tab, new_tab, new_max,esz,cmp,aux);
+      tab += esz;
+    }
+  }
+  //logtrace("reash completed");
+  return new_tab;
+}
+
+static void *utl_vec_add_hashed(vec_t v)
+{
+  uint8_t *elm;
+  uint8_t *tab;
+  uint32_t h;
+  
+  if (((v->cnt * 100) / 80) >= v->max) {  // Load factor max: 80%
+    //logtrace("Reash at %d",v->cnt);
+    tab = utl_reash(v->vec,v->max,v->esz,v->cmp,v);
+    if (tab == NULL) return NULL;
+    free(v->vec);
+    v->vec = tab;
+    v->max *= 2;
+  }
+  
+  h = v->hsh(v->elm,v) & 0x7FFFFFFF;
+  elm = v->elm;
+  utl_h(elm, v->esz) = h;
+  elm = utl_vec_hsh_set(elm,v->vec,v->max,v->esz,v->cmp,v);
+  
+  if (elm) v->cnt += 1;
+ 
+  return elm;
+}
+
+/* * sets * */
 void *utl_vec_add(vec_t v, uint32_t i) {
+  if (v->hsh) return utl_vec_add_hashed(v);
   if (v->cnt > 0) {
     if (v->cmp) return utl_vec_add_sorted(v);
   }
   return utl_vec_set(v,v->cnt);
-}
-
-int16_t utl_vec_del(vec_t v, uint32_t i)
-{
-  return utl_vec_delgap(v,i,1);
-}
-
-int16_t utl_vec_delrange(vec_t v, uint32_t i,  uint32_t j)
-{
-  if (j<i) return 0;
-  return utl_vec_delgap(v,i,j-i+1);  
 }
 
 size_t utl_vec_read(vec_t v,uint32_t i, size_t n,FILE *f)
@@ -485,31 +899,129 @@ size_t utl_vec_write(vec_t v, uint32_t i, size_t n, FILE *f)
   return fwrite(v->vec+(i*v->esz),v->esz,n,f);
 }
 
-void utl_vec_sort(vec_t v, int (*cmp)(void *, void *))
-{
-  if (cmp != utl_vec_nullcmp && cmp != v->cmp) {
-    v->cmp = cmp;
-    vecunsorted(v);
-  }
-  if (v->cmp == NULL) {
-    vecunsorted(v);
-  }
-  else if (!vecissorted(v)) {
-    if (v->cnt > 1)
-      qsort(v->vec,v->cnt,v->esz,(int (*)(const void *, const void *))(v->cmp));  
-    vecsorted(v);
-  }
-}
 
 void *utl_vec_search(vec_t v, int x)
 {
-  if (v->cmp) {
-    vecsort(v);
-    return bsearch(v->elm,v->vec,v->cnt,v->esz,(int (*)(const void *, const void *))(v->cmp));
+  uint8_t *elm = NULL;
+  
+  if (v->cnt == 0) return NULL;
+  
+  if (v->hsh) {
+    elm = utl_search_hashed(v);
   }
-  return NULL;
+  else if (v->cmp) {
+    elm = utl_vec_search_sorted(v);
+  }
+  return elm;
 }
 
+int utl_vec_remove(vec_t v, int x)
+{
+  uint8_t *elm = NULL;
+  uint32_t pos;
+  
+  if (v->cnt == 0) return 0;
+  
+  elm = utl_vec_search(v,0);
+  if (elm) {
+    pos = (elm - v->vec)/v->esz;
+    if (v->hsh) {
+      elm = utl_remove_hashed(v,pos);
+    } 
+    else {
+      if (!utl_vec_del(v,pos)) elm = NULL;
+    }
+  }
+  return (elm != NULL);
+}
+
+void *utl_vec_first(vec_t v)
+{
+  uint8_t  *elm = NULL;
+  
+  if (v->cnt == 0) {
+    v->cur = vec_MAX_CNT;
+    return NULL;
+  }
+  elm = v->vec;
+  if (v->fst != vec_MAX_CNT) {
+    elm = v->vec + v->fst * v->esz ;
+  }
+  else if (v->hsh) {
+     while (utl_h(elm,v->esz) == 0xFFFFFFFF) {
+       elm += v->esz;
+     }
+  }
+  v->cur = (elm - v->vec) / v->esz;
+  return elm;
+}
+
+void *utl_vec_last(vec_t v)
+{
+  uint8_t  *elm = NULL;
+  
+  if (v->cnt == 0) {
+    v->cur = vec_MAX_CNT;
+    return NULL;
+  }
+  elm = v->vec + (v->cnt-1) * v->esz;
+  if (v->lst != vec_MAX_CNT) {
+    elm = v->vec + v->lst * v->esz ;
+  }
+  else if (v->hsh) {
+    elm = v->vec + (v->max-1) * v->esz;
+    while (utl_h(elm,v->esz) == 0xFFFFFFFF) {
+      elm -= v->esz;
+    }
+  }
+  v->cur = (elm - v->vec) / v->esz;
+  return elm;
+}
+
+void *utl_vec_next(vec_t v)
+{
+  uint32_t max = v->cnt;
+  uint8_t *elm;
+  
+  if (v->cnt == 0) v->cur = vec_MAX_CNT;
+  if (v->cur == v->lst) v->cur = vec_MAX_CNT;
+  if (v->cur == vec_MAX_CNT) return NULL;
+  v->cur++;
+  elm = v->vec + v->cur * v->esz;
+  if (v->hsh) {
+    max = v->max;
+    while (v->cur < max && (utl_h(elm,v->esz) == 0xFFFFFFFF)) {
+      v->cur++;
+      elm = v->vec + v->cur * v->esz;
+    }
+  }
+  
+  if (v->cur >= max) v->cur = vec_MAX_CNT;
+  if (v->cur == vec_MAX_CNT) return NULL;
+  return elm;
+}
+
+void *utl_vec_prev(vec_t v)
+{
+  uint8_t *elm;
+  
+  if (v->cnt == 0) v->cur = vec_MAX_CNT;
+  if (v->cur == v->fst) v->cur = vec_MAX_CNT;
+  if (v->cur == 0) v->cur = vec_MAX_CNT;
+  if (v->cur == vec_MAX_CNT) return NULL;
+  v->cur--;
+  elm = v->vec + v->cur * v->esz;
+  if (v->hsh) {
+    while (v->cur > 0 && (utl_h(elm,v->esz) == 0xFFFFFFFF)) {
+      v->cur--;
+      elm = v->vec + v->cur * v->esz;
+    }
+    if (utl_h(elm,v->esz) == 0xFFFFFFFF) v->cur = vec_MAX_CNT;
+  }
+  
+  if (v->cur == vec_MAX_CNT) return NULL;
+  return elm;
+}
 
 
 /* ** BUF ********************* */
--- dist/utl.h
+++ dist/utl.h
@@ -29,6 +29,7 @@ extern "C" {
 #include <stddef.h>
 #include <time.h>
 #include <ctype.h>
+#include <assert.h>
 
 #ifdef NDEBUG
 
@@ -53,11 +54,19 @@ extern "C" {
 int   utl_ret(int x);
 void *utl_retptr(void *x);
 
+uint32_t utl_hash_string(void *key);
+uint32_t utl_hash_int32(void *key);
+
+#define utlhashstring  utl_hash_string
+#define utlhashint32   utl_hash_int32
+
 extern const char *utl_emptystring;
 
-#define utl_arg0(x1,...)        x1                                                                                                                             
+#define utl_arg0(x1,...)        x1
 #define utl_arg1(x1,x2,...)     x2
 #define utl_arg2(x1,x2,x3,...)  x3
+
+//uint32_t utl_rnd();
 
 #line 28 "src/utl_log.h"
 #ifndef UTL_NOLOG
@@ -190,17 +199,19 @@ size_t utl_mem_used (void);
 #define vec_MAX_CNT  UINT32_MAX
 
 typedef struct vec_s {
-  uint32_t  fst; 
-  uint32_t  lst; 
-  uint32_t  max;
-  uint32_t  cnt;
-  uint16_t  esz;  /* each element max size: 64K */
-  uint16_t  flg;
-  int     (*cmp)(void *, void *);
-  int     (*hsh)(void *);
-  uint8_t  *vec;
-  void     *elm; // this will always point to eld
-  uint8_t   eld[4];
+  uint32_t   fst; 
+  uint32_t   lst; 
+  uint32_t   cur; 
+  uint32_t   max;
+  uint32_t   cnt;
+  uint16_t   esz;  /* each element max size: 64K */
+  uint16_t   flg;
+  int      (*cmp)(void *, void *, void *);
+  uint32_t (*hsh)(void *, void *);
+  uint8_t   *vec;
+  void      *aux;
+  void      *elm; // this will always point to eld
+  uint8_t    eld[4];
 } vec_s, *vec_t;
 
 #define vecDO(type,v,i,e,x)  do {vec_t v_=v;  *((type *)(v_->elm)) = (e); x(v_,i);} while (0)
@@ -213,10 +224,12 @@ typedef struct vec_s {
 
 // Set (sorted or unsorted) 
 #define vecadd(type,v,e)     vecDO(type,v,vec_MAX_CNT,e,utl_vec_add)
-#define vecsearch(type,v,e)  (type *)utl_vec_search(v, (*((type *)(v->elm)) = (e), 0))  
+//#define vecsearch(type,v,e)  (type *)utl_vec_search(v, (*((type *)(v->elm)) = (e), 0))  
+#define vecsearch(type,v,e)  (*((type *)(v->elm)) = (e), (type *)utl_vec_search(v, 0))
+#define vecremove(type,v,e)  utl_vec_remove(v, (*((type *)(v->elm)) = (e), 0))  
 
 #define vecsort(...)  utl_vec_sort(utl_arg0(__VA_ARGS__,NULL), utl_arg1(__VA_ARGS__,utl_vec_nullcmp,NULL))
-                                                                                                                                                  
+
 #define vecSORTED 0x0001
 #define vecHASHED 0x0020
 
@@ -238,12 +251,19 @@ typedef struct vec_s {
 #define vecwrite(v,i,n,f) utl_vec_write(v,i,n,f)
 
 // Info
-#define vecnew(type)   utl_vec_new(sizeof(type))
-#define vecfree(v)     utl_vec_free(v)
-#define veccount(v)    ((v)->cnt)
-#define vecmax(v)      ((v)->max)
-#define vecisempty(v)  ((v)->cnt == 0)
-#define vec(type,v)    ((type *)((v)->vec))
+#define vecnew(...)       utl_vec_new(sizeof(utl_arg0(__VA_ARGS__,int)),utl_arg1(__VA_ARGS__,NULL,NULL),utl_arg2(__VA_ARGS__,NULL,NULL,NULL))
+#define vecfree(v)        utl_vec_free(v)
+#define veccount(v)       ((v)->cnt)
+#define vecmax(v)         ((v)->max)
+#define vecaux(v)         ((v)->aux)
+#define vecisempty(v)     ((v)->cnt == 0)
+#define vec(type,v)       ((type *)((v)->vec))
+#define vecclear(v)       ((v)->cnt = 0)
+
+#define vecfirst(v)       utl_vec_first(v)
+#define vecnext(v)        utl_vec_next(v)
+#define vecprev(v)        utl_vec_prev(v)
+#define veclast(v)        utl_vec_last(v)
 
 // Protypes
 void *utl_vec_set(vec_t v, uint32_t i);
@@ -251,23 +271,30 @@ void *utl_vec_ins(vec_t v, uint32_t i);
 void *utl_vec_add(vec_t v, uint32_t i);
 void *utl_vec_get(vec_t v, uint32_t i);
 
+void *utl_vec_first(vec_t v);
+void *utl_vec_next(vec_t v) ;
+void *utl_vec_prev(vec_t v) ;
+void *utl_vec_last(vec_t v) ;
+
 int16_t utl_vec_del(vec_t v,  uint32_t i);
 int16_t utl_vec_delrange(vec_t v, uint32_t i,  uint32_t j);
 
-vec_t utl_vec_new(uint16_t esz);
+vec_t utl_vec_new(uint16_t esz, int (*cmp)(void *, void *, void *), uint32_t (*hsh)(void *, void *));
 vec_t utl_vec_free(vec_t v);
 
 size_t utl_vec_read(vec_t v,uint32_t i, size_t n,FILE *f);
 size_t utl_vec_write(vec_t v, uint32_t i, size_t n, FILE *f);
 
-void utl_vec_sort(vec_t v, int (*cmp)(void *, void *));
+void utl_vec_sort(vec_t v, int (*cmp)(void *, void *, void *));
 void *utl_vec_search(vec_t v,int x);
+int utl_vec_remove(vec_t v, int x);
 
-int utl_vec_nullcmp(void *a, void *b);
+int utl_vec_nullcmp(void *a, void *b, void *aux);
 
 // Character buffer
 #define buf_t                 vec_t
 #define bufnew()              vecnew(char)
+#define buffree(b)            vecfree(b)
 #define bufaddc(b,c)          vecpush(char,b,c)
 #define bufsetc(b,i,c)        vecset(char,b,i,c)
 #define bufinsc(b,i,c)        utl_buf_insc(b,i,c)
@@ -280,8 +307,11 @@ int utl_vec_nullcmp(void *a, void *b);
 #define bufreadln(b,i,f)      utl_buf_readln(b,i,f)
 #define bufreadall(b,i,f)     utl_buf_readall(b,i,f)
 #define bufwrite(b,i,n,f)     vecwrite(b,i,n,f)
-#define buf(b)                vec(char, b)
+#define buf(b)                vec(char, (vec_t)b)
 #define buflen(b)             veccount(b)
+
+#define bufaux(b)             vecaux(b)
+#define bufcur(b)            ((b)->cur)
 
 char utl_buf_get(buf_t b, uint32_t n);
 size_t utl_buf_readall(buf_t b, uint32_t i, FILE *f);
@@ -291,6 +321,9 @@ char *utl_buf_sets(buf_t b, uint32_t i, const char *s);
 char *utl_buf_inss(buf_t b, uint32_t i, const char *s);
 char *utl_buf_insc(buf_t b, uint32_t i, char c);
 int16_t utl_buf_del(buf_t b, uint32_t i,  uint32_t j);
+
+void utl_dpqsort(void *base, uint32_t nel, uint32_t esz, int (*cmp)(const void *, const void *, const void *), void *aux);
+#define utlqsort(b,n,s,c,x) utl_dpqsort(b,n,s,c,x)
 
 #endif 
 #line 20 "src/utl_pmx.h"
