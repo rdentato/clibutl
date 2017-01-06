a78975a218b808d75c7d059c8176e64aa25218a2
rdentato <rdentato@gmail.com>
2017-01-06 13:27:37 +0100

    Tests for Double Pivot Quicksort

---
+++ test/ut_dpq.c
@@ -0,0 +1,126 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+**     
+**                        ___   __
+**                     __/  /_ /  )
+**             ___  __(_   ___)  /
+**            /  / /  )/  /  /  /
+**           /  (_/  //  (__/  / 
+**          (____,__/(_____(__/
+**    https://github.com/rdentato/clibutl
+**
+*/
+#define UTL_MEMCHECK
+#include "utl.h"
+#include <math.h>
+
+typedef struct point_s {
+  float x,y;
+} point_t;
+
+typedef struct {
+  char *k;
+  int   v;
+} mapSI_t;
+
+int intcmp(void *a, void *b, void *aux)
+{
+  int ia = *((int *)a);
+  int ib = *((int *)b);
+  //logtrace("CMP: %d %d",ia,ib);
+  return (ia - ib);
+}
+
+void logtable(vec_t v)
+{
+  int *pk;
+  int k =0;
+  pk = vecfirst(v);
+  while (pk) {
+    logprintf("V[%d] -> %d",k++,*pk);
+    pk = vecnext(v);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  
+  vec_t v;
+  //int *pk;
+  int k;
+  
+  logopen("l_dpq.log","w");
+  
+  v=vecnew(int);
+  logcheck(veccount(v) == 0);
+  
+  vecset(int,v,0,37);
+  vecset(int,v,1,5);
+  vecset(int,v,2,79);
+
+  logtable(v);
+  
+  vecsort(v,intcmp);
+  logcheck(vecsorted(v));
+  
+  logtable(v);
+  
+  vecset(int,v,0,3);
+  vecset(int,v,1,57);
+  vecset(int,v,2,79);
+
+  logtable(v);
+  
+  vecsort(v,intcmp);
+  logcheck(vecsorted(v));
+  
+  logtable(v);
+  logtrace("random vector (small)");
+  srand(time(0));
+  for (k=0;k<=18;k++) {
+    vecset(int,v,k,((rand() & 0xF) <<2)+k);
+  }
+  
+  logclock {
+    vecsort(v);
+  }
+  logtable(v);
+  
+  
+  
+#if 1
+  logtrace("random vector (large)");
+  
+  #define N 100000
+  for (k=0;k<=N;k++) {
+    vecset(int,v,k,((rand() & 0xF) <<24)+k);
+  }
+  logtrace("done (%d on %d)",veccount(v), vecmax(v));
+  
+  //logtable(v);
+  
+  logclock {
+    vecsort(v);
+  }
+  //logtable(v);
+  
+  logtrace("sorted vector");
+  for (k=0;k<=N;k++) {
+    vecset(int,v,k,10000000+k);
+  }
+
+  //logtable(v);
+  logtrace("done");
+  
+  logclock {
+    vecsort(v);
+  }
+  //logtable(v);
+#endif  
+  vecfree(v);
+  logclose();
+  exit(0);
+}
