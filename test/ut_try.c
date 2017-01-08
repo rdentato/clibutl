919c5ee87f295a2efc2016d4f3c2ba243a136c32
rdentato <rdentato@gmail.com>
2017-01-08 16:34:20 +0100

    Added rethrow() and thrown()

--- test/ut_try.c
+++ test/ut_try.c
@@ -25,7 +25,7 @@
 
 void myfunc(int ex)
 {
-  logprintf("Throwing %d",ex);
+  logprintf("    Throwing %d",ex);
   throw(ex);
 }
 
@@ -35,115 +35,141 @@ int main(int argc, char *argv[])
   int k = 0;
   
   logopen("l_try.log","w");
-  
+#if 1  
   try {
-    logprintf("No exception");
+    logprintf("    No exception");
     k=1;
   }
   catchall {
-    logprintf("Catch all");
+    logprintf("   Catch all");
     k = 9;    
   }
   logcheck(k==1);
   
   try {
-    logprintf("Exception NOFILE");
+    logprintf("     Exception NOFILE");
     throw(NOFILE);
   }
   catch(NOFILE) {
-    k = 20;  logprintf("NO FILE FOUND");
+    k = 20;  logprintf("    NO FILE FOUND");
   }  
   catch(OUTOFMEM) {
-    k = 10;  logprintf("OUT OF MEMORY");
+    k = 10;  logprintf("    OUT OF MEMORY");
   }  
   catchall {
-    k = 30;  logprintf("DON'T KNOW WHAT'S WRONG!");
+    k = 30;  logprintf("    DON'T KNOW WHAT'S WRONG!");
   }
   logcheck(k==20);
  
   try {
-    logprintf("Exception NOFILE");
+    logprintf("    Exception NOFILE");
     throw(OUTOFMEM);
   }
   catch(NOFILE) {
-    k = 20;  logprintf("NO FILE FOUND");
+    k = 20;  logprintf("    NO FILE FOUND");
   }  
   catch(OUTOFMEM) {
-    k = 10;  logprintf("OUTOFMEM");
+    k = 10;  logprintf("    OUTOFMEM");
   }  
   catchall {
-    k = 30;  logprintf("DON'T KNOW WHAT'S WRONG!");
+    k = 30;  logprintf("    DON'T KNOW WHAT'S WRONG!");
   }
   logcheck(k==10);
  
   try {
-    logprintf("Exception unhandled");
+    logprintf("    Exception unhandled");
     throw(PANIC);
   }
   catch(NOFILE) {
-    k = 20;  logprintf("NO FILE FOUND");
+    k = 20;  logprintf("    NO FILE FOUND");
   }  
   catch(OUTOFMEM) {
-    k = 10;  logprintf("OUTOFMEM");
+    k = 10;  logprintf("    OUTOFMEM");
   }  
   catchall {
-    k = 30;  logprintf("DON'T KNOW WHAT'S WRONG!");
+    k = 30;  logprintf("    DON'T KNOW WHAT'S WRONG!");
   }
   logcheck(k==30);
   
   k = 0;
   try {
-    logprintf("Exception unhandled (no catch all)");
+    logprintf("    Exception unhandled (no catch all)");
     k=7; // WARNING! Local vars will be reverted to the previous value
     throw(PANIC);
     k=9; // Won't be executed
   }
   catch(NOFILE) {
-    k = 20;  logprintf("NO FILE FOUND");
+    k = 20;  logprintf("    NO FILE FOUND");
   }  
   catch(OUTOFMEM) {
-    k = 10;  logprintf("OUTOFMEM");
+    k = 10;  logprintf("    OUTOFMEM");
   }  
-  if (!logcheck(k==0)) logprintf("k: %d",k);
+  // At this point k is undefined!! 
+  //(with GCC would be 0, with MSVC would be 7)
+  logprintf("    undefined k: %d",k);
+#endif
  
-
-  k = 0;
+  k = 652;
   try {
-    logprintf("throw from a function");
+    logprintf("    throw from a function");
+    logtrace("jb+: %p",(void *)&utl_jb);
     myfunc(OUTOFMEM);
+    logtrace("     SHOULDN'T BE HERE");
   }
   catch(NOFILE) {
-    k = 20;  logprintf("NO FILE FOUND");
+    k = 20;  logprintf("    NO FILE FOUND");
   }  
   catch(OUTOFMEM) {
-    k = 10;  logprintf("OUTOFMEM");
+    k = 10;  logprintf("    OUTOFMEM");
   }  
   catchall {
-    k = 30;  logprintf("DON'T KNOW WHAT'S WRONG!");
+    k = 30;  logprintf("    DON'T KNOW WHAT'S WRONG!");
   }
-  logcheck(k==10);
+  if (!logcheck(k==10)) logprintf("k: %d",k);
 
   k = 0;
   try {
-    logprintf("Nested try");
+    logprintf("    Nested try");
     try {
-      throw(NOFILE);
+      myfunc(NOFILE);
     }
     catch(NOFILE) {
       k=2;
     }
   }
   catch(NOFILE) {
-    k = 20;  logprintf("NO FILE FOUND");
+    k = 20;  logprintf("    NO FILE FOUND");
   }  
   catch(OUTOFMEM) {
-    k = 10;  logprintf("OUTOFMEM");
+    k = 10;  logprintf("    OUTOFMEM");
   }  
   catchall {
-    k = 30;  logprintf("DON'T KNOW WHAT'S WRONG!");
+    k = 30;  logprintf("    DON'T KNOW WHAT'S WRONG!");
   }
   logcheck(k==2);
 
+  k = 0;
+  try {
+    logprintf("    Nested try");
+    try {
+      throw(OUTOFMEM);
+    }
+    catch(NOFILE) {
+      k=2;
+    }
+    catchall { k=3;rethrow();}
+  }
+  catch(NOFILE) {
+    k = 20;  logprintf("    NO FILE FOUND");
+  }  
+  catch(OUTOFMEM) {
+    k = 10;  logprintf("    OUTOFMEM");
+  }  
+  catchall {
+    k = 30;  logprintf("    DON'T KNOW WHAT'S WRONG!");
+  }
+  logcheck(k==10);
+  
 
   logclose();
   exit(0);
