94cef183b14ee272e412f735d8ee2490191e3e47
rdentato <rdentato@gmail.com>
2017-01-10 14:54:40 +0100

    Test logging level

---
+++ test/ut_log.c
@@ -0,0 +1,59 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+**     
+**                        ___   __
+**                     __/  /_ /  )
+**             ___  __(_   ___)  /
+**            /  / /  )/  /  /  /
+**           /  (_/  //  (__/  / 
+**          (____,__/(_____(__/
+**    https://github.com/rdentato/clibutl
+**
+*/
+
+#include "utl.h"
+
+int main(int argc, char *argv[])
+{
+  logopen("l_log.log","w");
+
+  logprintf("This line will always be printed");
+  
+  logtracewatch("(info)","(warning)","(error)") {
+    loginfo("This line is expected to be in the log (info)");
+    logwarning("This line is expected to be in the log (warning)");
+    logerror("This line is expected to be in the log (error)");
+  }
+  
+  loglevel("WARNING");
+  logexpect(utl_log_prdlvl == UTL_LOG_W,"Log Level: %d",utl_log_prdlvl);
+  
+  logtracewatch("<not>(info)","(warning)","(error)") {
+    loginfo("This line is NOT expected to be in the log (info)");
+    logwarning("This line is expected to be in the log (warning)");
+    logerror("This line is expected to be in the log (error)");
+  }
+  
+  loglevel("ERROR");
+  logexpect(utl_log_prdlvl == UTL_LOG_E,"Log Level: %d",utl_log_prdlvl);
+  
+  logtracewatch("<not>(info)","<not>(warning)","(error)") {
+    loginfo("This line is NOT expected to be in the log (info)");
+    logwarning("This line is NOT expected to be in the log (warning)");
+    logerror("This line is expected to be in the log (error)");
+  }
+  
+  loglevel("NONE");
+  logexpect(utl_log_prdlvl == UTL_LOG_N,"Log Level: %d",utl_log_prdlvl);
+  
+  logtracewatch("<not>(info)","<not>(warning)","<not>(error)") {
+    loginfo("This line is NOT expected to be in the log (info)");
+    logwarning("This line is NOT expected to be in the log (warning)");
+    logerror("This line is expected to be in the log (error)");
+  }
+  
+  logclose();
+}
