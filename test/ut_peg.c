623b9f8f9244dd30b1863438a761ae0abf5faeb3
rdentato <rdentato@gmail.com>
2017-02-11 22:35:09 +0100

    Tests for PEG

---
+++ test/ut_peg.c
@@ -0,0 +1,136 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+**     
+**                        ___   __
+**                     __/  /_ /  )
+**             ___  __(_   ___)  /
+**            /  / /  )/  /  /  /
+**           /  (_/  //  (__/  / 
+**          (____,__/(_____(__/
+**    https://github.com/rdentato/clibutl
+**
+*/
+
+#include "utl.h"
+
+/* 
+   S =  (('+'|'-')? V)+
+   V = upper+ | digit+
+
+*/
+
+pegrule(S) {
+  pegmore {
+    pegref(SPC);
+    pegdefer(op) {
+      pegopt{pegoneof("+-");}
+    }
+    pegref(SPC);
+    pegdefer(arg) {
+      pegref(V);
+    }
+  }
+}
+
+pegrule(V){
+  pegref(SPC);
+  pegalt {
+    pegor{ pegmore {pegupper;}}
+    pegor{ pegmore {pegdigit;}}
+  }  
+}
+
+pegrule(SPC) {
+  pegstar{pegoneof(" \t");}
+}
+
+
+  /*
+  expr   := term ('+' | '-') expr | term
+  term   := fact ('*' | '/') term | fact
+  fact   := '(' expr ')' | num
+  */
+pegrule(expr) {
+  pegswitch {
+    pegcase {
+      pegref(term);
+      pegdefer(op) {
+        pegoneof("+-");
+      }
+      pegref(expr);
+    }
+    pegcase {
+      pegref(term);
+    }
+  }
+}
+
+pegrule(term) {
+  pegswitch {
+    pegcase {
+      pegref(fact);
+      pegdefer(op) {
+        pegoneof("*/");
+      }
+      pegref(term);
+    }
+    pegcase {
+      pegref(fact);
+    }
+  }
+}
+
+pegrule(fact) {
+  pegswitch {
+    pegcase {
+      pegdefer(PUSH);
+      pegstr("("); pegref(expr); pegstr(")");
+      pegdefer(POP);
+    }
+    pegcase {
+      pegdefer(argnum) {
+        pegstar{ pegdigit; } 
+      }
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+  char *q;
+  peg_t pg;
+  
+  logopen("l_peg.log","w");
+  pg = pegnew();
+  
+  q = "ABCDE+FEG-43+AA";
+  fprintf(stderr,"\nexpr: '%s'\n",q);
+  logcheck(pegparse(pg,S,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  logprintf("match: %.*s",(int)(pg->pos - q),q);
+
+  q = "ABCDE + FEG - 43+AA";
+  fprintf(stderr,"\nexpr: '%s'\n",q);
+  logcheck(pegparse(pg,S,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  logprintf("match: %.*s",(int)(pg->pos - q),q);
+  
+  fprintf(stderr,"\n****\n");
+  char *e[] = {"3+2","3-4*2","(3-2)*(4/2)",NULL};
+  char **qe;
+  qe = e; 
+  for (q = *qe; q; q=*(++qe)) {
+    fprintf(stderr,"\n%s\n",q);
+    logcheck(pegparse(pg,expr,q));
+    logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+    logprintf("match: %.*s",(int)(pg->pos - q),q);
+  }
+  
+  pg = pegfree(pg);
+  logclose();
+  exit(0);
+}
---
+++ test/ut_peg2.c
@@ -0,0 +1,189 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+**     
+**                        ___   __
+**                     __/  /_ /  )
+**             ___  __(_   ___)  /
+**            /  / /  )/  /  /  /
+**           /  (_/  //  (__/  / 
+**          (____,__/(_____(__/
+**    https://github.com/rdentato/clibutl
+**
+*/
+
+#include "utl.h"
+
+/* 
+   S = upper* digit
+*/
+
+pegrule(S1) {
+  pegdo(prt) {
+    pegmore { pegupper; }
+    pegdigit;
+  }
+}
+
+pegrule(S2) {
+  pegmore {
+    pegdo(prt) {
+      pegupper;
+    }
+  }
+  pegdo(prt) {
+    pegdigit;
+  }
+}
+
+/*
+   S = upper S | digit
+   
+*/
+
+pegrule(S3) {
+  pegdo(prt) {
+    pegalt { 
+      pegor {
+        pegupper;
+        pegref(S3);
+      }
+      pegor {
+        pegdigit;
+      }
+    }
+  }
+}
+
+pegrule(S4) {
+  pegalt{
+    pegor {
+      pegupper;
+      pegdo(prt) {
+        pegref(S4);
+      }
+    }
+    pegor {
+      pegdigit;
+    }
+  }
+}
+
+pegrule(S5) {
+  // PIPPO | PLUTO
+  pegstr("P");
+  pegalt {
+    pegor{ pegstr("IPPO"); }
+    pegor{ pegstr("LUTO"); }
+  }
+}
+
+/*
+   A = '(' A ')' | ('+'|'-')? B
+   B = NUM
+*/
+
+pegrule(A) {
+  pegalt {
+    pegeither {
+      pegstr("("); pegref(A); pegstr(")");
+    }
+    pegor {
+      pegopt{
+        pegoneof("+-");
+      }
+      pegref(B);
+    }
+  }
+}
+
+pegrule(B) {
+  pegmore { pegdigit; }
+}
+
+int prt(const char *from, const char *to, void *aux)
+{
+  logprintf("(%.*s)",(int)(to-from),from);
+  return 0;
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+  
+  char *q;
+  peg_t pg;
+  
+  logopen("l_peg2.log","w");
+ 
+  pg = pegnew();
+ 
+  q = "ABCD4";
+  
+  logcheck(pegparse(pg,S1,q));
+  logcheck(*(pg->pos) == '\0');
+  
+  logcheck(pegparse(pg,S2,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+
+  logcheck(pegparse(pg,S3,q));
+
+  logcheck(pegparse(pg,S4,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  
+  q = "ABCD";
+  logcheck(!pegparse(pg,S2,q));
+  logprintf("q:%p p:%p m:%p c:%d line:%d col:%d rule:%s ",q,pg->pos,pg->errpos,*pg->errpos,pg->errln, pg->errcn,pg->errrule);
+  
+  q = "PIPPO";
+  logcheck(pegparse(pg,S5,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  
+  q = "PLUTO";
+  logcheck(pegparse(pg,S5,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  
+  q = "PLUKA";
+  logcheck(!pegparse(pg,S5,q));
+  logprintf("q:%p p:%p m:%p c:%d line:%d col:%d rule:%s ",q,pg->pos,pg->errpos,*pg->errpos,pg->errln, pg->errcn,pg->errrule);
+  
+  q="34";
+  logcheck(pegparse(pg,A,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  logprintf("match: %.*s",(int)(pg->pos - q),q);
+  
+  q="(34)";
+  logcheck(pegparse(pg,A,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  logprintf("match: %.*s",(int)(pg->pos - q),q);
+  
+  q="(34)-43+65";
+  logcheck(pegparse(pg,A,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  logprintf("match: %.*s",(int)(pg->pos - q),q);
+  
+  q="3+4";
+  
+  logcheck(pegparse(pg,expr,q));
+  logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+  logprintf("match: %.*s",(int)(pg->pos - q),q);
+
+  fprintf(stderr,"\n****\n");
+  char *e[] = {"3+2","3-4*2",NULL};
+  char **qe;
+  qe = e; 
+  for (q = *qe; q; q=*(++qe)) {
+    fprintf(stderr,"\n%s\n",q);
+    logcheck(pegparse(pg,expr,q));
+    logprintf("q:%p p:%p m:%p c:%d",q,pg->pos,pg->errpos,*pg->errpos);
+    logprintf("match: %.*s",(int)(pg->pos - q),q);
+  }
+  
+  pg = pegfree(pg);
+  logclose();
+  exit(0);
+}
